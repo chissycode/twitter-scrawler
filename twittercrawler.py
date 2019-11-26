a29048802372547954891bb80e059d7bafe22883
Ang Li <948585958@qq.com>
2019-11-26 18:46:24 -0500

    Add files via upload

---
+++ twittercrawler.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 17:27:00 2019
+
+@author: liang
+"""
+
+
+import twitter
+import json
+import sys
+import os
+import time
+from urllib.error import URLError
+from http.client import BadStatusLine
+
+
+#read user id from .txt file to a list
+def read_file(filename):
+    with open(filename,'r') as f:
+        data= f.readlines()
+        id_list=[]
+    
+    for line in data:
+        num_id=line.split('::')
+        userid=num_id[1]
+        id_list.append(userid)
+
+    id_list=[i.strip('\n') for i in id_list]    
+    return id_list
+
+#write a list to .txt file
+def write_file(filename,list):        
+    file_write_obj = open(filename, 'a')
+    for var in list:
+        file_write_obj.writelines(var)
+        file_write_obj.write('\n')
+    file_write_obj.close()
+
+def write_file_userid(filename,num,userid):
+    file_write_obj = open(filename, 'a')
+    file_write_obj.write('::')
+    file_write_obj.write(str(num))
+    file_write_obj.write('::')
+    file_write_obj.write(userid)
+    file_write_obj.write('::')
+    file_write_obj.write('\n')
+    file_write_obj.close()
+    
+def remove_file(filename):
+    if os.path.exists(filename): 
+        os.remove(filename)
+
+def oauth_login():
+    # XXX: Go to http://twitter.com/apps/new to create an app and get values
+    # for these credentials that you'll need to provide in place of these
+    # empty string values that are defined as placeholders.
+    # See https://developer.twitter.com/en/docs/basics/authentication/overview/oauth
+    # for more information on Twitter's OAuth implementation.
+    
+    CONSUMER_KEY = 'pqpte5I78XrkcB3Q2kTqoHHlQ'
+    CONSUMER_SECRET = 'Ki7W4EKTYLbMbIUq7pWk7z1RVmKRCwsGqN01K36KRyzuSPqN6n'
+    OAUTH_TOKEN = '1091357686686511105-OBbDWT5bb8Thf9T89isnwp9AB9NRCZ'
+    OAUTH_TOKEN_SECRET = 'g9pxNU4UTGvSoWdu2pIeSQDzrL1yUv0zSKV3dvUCBzsgw'
+    
+    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                               CONSUMER_KEY, CONSUMER_SECRET)
+    
+    twitter_api = twitter.Twitter(auth=auth)
+    return twitter_api
+
+
+
+def make_twitter_request(twitter_api_func, max_errors=10, *args, **kw): 
+    
+    # A nested helper function that handles common HTTPErrors. Return an updated
+    # value for wait_period if the problem is a 500 level error. Block until the
+    # rate limit is reset if it's a rate limiting issue (429 error). Returns None
+    # for 401 and 404 errors, which requires special handling by the caller.
+    def handle_twitter_http_error(e, wait_period=2, sleep_when_rate_limited=True):
+    
+        if wait_period > 3600: # Seconds
+            print('Too many retries. Quitting.', file=sys.stderr)
+            raise e
+    
+        # See https://developer.twitter.com/en/docs/basics/response-codes
+        # for common codes
+    
+        if e.e.code == 401:
+            print('Encountered 401 Error (Not Authorized)', file=sys.stderr)
+            return None
+        elif e.e.code == 404:
+            print('Encountered 404 Error (Not Found)', file=sys.stderr)
+            return None
+        elif e.e.code == 429: 
+            print('Encountered 429 Error (Rate Limit Exceeded)', file=sys.stderr)
+            if sleep_when_rate_limited:
+                print("Retrying in 15 minutes...ZzZ...", file=sys.stderr)
+                sys.stderr.flush()
+                time.sleep(60*15 + 5)
+                print('...ZzZ...Awake now and trying again.', file=sys.stderr)
+                return 2
+            else:
+                raise e # Caller must handle the rate limiting issue
+        elif e.e.code in (500, 502, 503, 504):
+            print('Encountered {0} Error. Retrying in {1} seconds'\
+                  .format(e.e.code, wait_period), file=sys.stderr)
+            time.sleep(wait_period)
+            wait_period *= 1.5
+            return wait_period
+        else:
+            raise e
+
+    # End of nested helper function
+    
+    wait_period = 2 
+    error_count = 0 
+
+    while True:
+        try:
+            return twitter_api_func(*args, **kw)
+        except twitter.api.TwitterHTTPError as e:
+            error_count = 0 
+            wait_period = handle_twitter_http_error(e, wait_period)
+            if wait_period is None:
+                return
+        except URLError as e:
+            error_count += 1
+            time.sleep(wait_period)
+            wait_period *= 1.5
+            print("URLError encountered. Continuing.", file=sys.stderr)
+            if error_count > max_errors:
+                print("Too many consecutive errors...bailing out.", file=sys.stderr)
+                raise
+        except BadStatusLine as e:
+            error_count += 1
+            time.sleep(wait_period)
+            wait_period *= 1.5
+            print("BadStatusLine encountered. Continuing.", file=sys.stderr)
+            if error_count > max_errors:
+                print("Too many consecutive errors...bailing out.", file=sys.stderr)
+                raise
+                
+                
+                
+
+def harvest_user_timeline(twitter_api, screen_name=None, user_id=None, max_results=1000):
+     
+    assert (screen_name != None) != (user_id != None), \
+    "Must have screen_name or user_id, but not both"    
+    
+    kw = {  # Keyword args for the Twitter API call
+        'count': 200,
+        'trim_user': 'true',
+        'include_rts' : 'true',
+        'since_id' : 1
+        }
+    
+    if screen_name:
+        kw['screen_name'] = screen_name
+    else:
+        kw['user_id'] = user_id
+        
+    max_pages = 16
+    results = []
+    
+    tweets = make_twitter_request(twitter_api.statuses.user_timeline, **kw)
+    
+    if tweets is None: # 401 (Not Authorized) - Need to bail out on loop entry
+        tweets = []
+        
+    results += tweets
+    
+    print('Fetched {0} tweets'.format(len(tweets)), file=sys.stderr)
+    
+    page_num = 1
+    
+    # Many Twitter accounts have fewer than 200 tweets so you don't want to enter
+    # the loop and waste a precious request if max_results = 200.
+    
+    # Note: Analogous optimizations could be applied inside the loop to try and 
+    # save requests. e.g. Don't make a third request if you have 287 tweets out of 
+    # a possible 400 tweets after your second request. Twitter does do some 
+    # post-filtering on censored and deleted tweets out of batches of 'count', though,
+    # so you can't strictly check for the number of results being 200. You might get
+    # back 198, for example, and still have many more tweets to go. If you have the
+    # total number of tweets for an account (by GET /users/lookup/), then you could 
+    # simply use this value as a guide.
+    
+    if max_results == kw['count']:
+        page_num = max_pages # Prevent loop entry
+    
+    while page_num < max_pages and len(tweets) > 0 and len(results) < max_results:
+    
+        # Necessary for traversing the timeline in Twitter's v1.1 API:
+        # get the next query's max-id parameter to pass in.
+        # See https://dev.twitter.com/docs/working-with-timelines.
+        kw['max_id'] = min([ tweet['id'] for tweet in tweets]) - 1 
+    
+        tweets = make_twitter_request(twitter_api.statuses.user_timeline, **kw)
+        results += tweets
+
+        print('Fetched {0} tweets'.format(len(tweets)),file=sys.stderr)
+    
+        page_num += 1
+        
+    print('Done fetching tweets', file=sys.stderr)
+
+    return results[:max_results]
+
+
+
+
+
+userid_list=read_file('users.txt')
+print(userid_list)
+
+twitter_api = oauth_login()
+
+num=0
+for userid in userid_list:   
+    num+=1
+    print('User#',num)
+    tweets=harvest_user_timeline(twitter_api,user_id=userid,max_results=200) 
+    tweet_text=[tweet['text'] for tweet in tweets]
+    write_file_userid('usertweet.txt',num,userid)
+    write_file('usertweet.txt',tweet_text)
+       #tweet_usermention=[user_mention['screen_name'] for user_mention in tweet['entities']['user_mentions']]
+       #tweet_hashtag=[hashtag['text'] for hashtag in tweet['entities']['hashtags']]
+       #retweeted_text=[tweet['retweeted_status']['text']]
+       #retweeted_usermention=[user_mention['screen_name'] for user_mention in tweet['retweeted_status']['entities']['user_mentions']]
+       #retweeted_hashtag=[hashtag['text'] for hashtag in tweet['retweeted_status']['entities']['hashtags']]
+       #'RT @TwitterDev: 1/ Today we’re sharing our vision for the future of the Twitter API platform!nhttps://t.co/XweGngmxlP,
+       #TwitterDev,
+       #1/ Today we’re sharing our vision for the future of the Twitter API platform!nhttps://t.co/XweGngmxlP'
+    
